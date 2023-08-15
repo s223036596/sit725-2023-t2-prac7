bf150a5a976e15984f0329d56a57afac9aa9b1a7
s223036596 <139842018+s223036596@users.noreply.github.com>
2023-08-15 23:31:14 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,51 +1,55 @@
-const cardList = [{
-    title: 'cat 2',
-    path: 'images/cat2.png',
-    subTitle: 'About Cat 2',
-    description: 'Description of Cat 2'
-},
-{
-    title: 'cat 3',
-    path: 'images/cat3.png',
-    subTitle: 'About Cat 3',
-    description: 'Description of Cat 3'
-},
-{
-    title: 'cat 4',
-    path: 'images/cat4.png',
-    subTitle: 'About Cat 4',
-    description: 'Description of Cat 4'
-}];
-
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">'+
                 '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="https://media.istockphoto.com/id/1322123064/photo/portrait-of-an-adorable-white-cat-in-sunglasses-and-an-shirt-lies-on-a-fabric-hammock.jpg?s=612x612&w=0&k=20&c=-G6l2c4jNI0y4cenh-t3qxvIQzVCOqOYZNvrRA7ZU5o=">'+
                 '</div><div class="card-content">'+
-                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#"></a></p></div>'+
                 '<div class="card-reveal">'+
-                '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-                '<p class="card-text">'+item.desciption+'</p>'+
+                '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
+                '<p class="card-text">'+item.description+'</p>'+
                 '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
 
-const formSumitted = () => {
+const formSubmitted = () => {
     let formData = {};
-    formData.firstName = $('#first_name').val();
-    formData.lastName = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.subTitle = $('#subTitle').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
 
     console.log(formData);
+    postCat(formData);
+}
+
+function postCat(cat){
+    $.ajax({
+        url:'/api/cat',
+        type:'POST',
+        data:cat,
+        success: (result)=>{
+            if (result.statusCode === 201) {
+                alert('cat post successful');
+            }
+        }
+    });
+}
+
+function getAllCats(){
+    $.get('/api/cats', (response)=>{
+        // response's data is in array format, so we can use it
+        if (response.statusCode === 200) {
+            addCards(response.data);
+        }
+    });
 }
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('#formSubmit').click(()=>{
-        formSumitted();
+        formSubmitted();
     });
-    addCards(cardList);
     $('.modal').modal();
+    getAllCats();
 });
